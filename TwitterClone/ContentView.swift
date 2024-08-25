af2b225f5a4bfd0256c50f615cce244e4e141739
farhan haider <f4farhann@gmail.com>
2024-08-25 23:55:06 +0530

    new screen added

--- TwitterClone/ContentView.swift
+++ TwitterClone/ContentView.swift
@@ -9,19 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "bird.fill")
-                .imageScale(.large)
-                .foregroundStyle(.blue)
-            Spacer()
-            Text("See what's happening in the world right now.")
-                .bold()
-                .font(.largeTitle)
-            Spacer()
-
-            Button(action: {}){
-                Text("Continue with Google")
-                    .font(.headline)
+        NavigationStack {
+            VStack {
+                Image(systemName: "bird.fill")
+                    .imageScale(.large)
+                    .foregroundStyle(.blue)
+                Spacer()
+                Text("See what's happening in the world right now.")
+                    .bold()
+                    .font(.largeTitle)
+                Spacer()
+                
+                // Button continue with google
+                Button(action: {}){
+                    HStack{
+                        Image("google_logo")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 20, height: 20)
+                        
+                        Text("Continue with Google")
+                            .font(.headline)
+                    }
                     .padding()
                     .frame(maxWidth: .infinity)
                     .background(Color.white)
@@ -30,53 +39,56 @@ struct ContentView: View {
                         RoundedRectangle(cornerRadius: 50)
                             .stroke(Color.gray, lineWidth: 2)
                     )
-            }.padding(.top)
-            Button(action: {}){
-                HStack{
-                    Image(systemName: "google_logo")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 20, height: 20) // Set the icon size
-
-                        .font(.headline)
-                    Text("Continue with Apple")                    .font(.headline)
-                }
-                .padding()
-                .frame(maxWidth: .infinity)
+                }.padding(.top)
+                
+                // Button continue with apple
+                Button(action: {}){
+                    HStack{
+                        Image("apple_logo")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 20, height: 20)
+                        Text("Continue with Apple")
+                            .font(.headline)
+                    }
+                    .padding()
+                    .frame(maxWidth: .infinity)
                     .background(Color.white)
                     .foregroundColor(.black)
                     .overlay(
-                    RoundedRectangle(cornerRadius: 50)
-                        .stroke(Color.gray, lineWidth: 2)
+                        RoundedRectangle(cornerRadius: 50)
+                            .stroke(Color.gray, lineWidth: 2)
                     )
-            }.padding(.top)
-            
-            Text("or")
-            
-            Button(action: {}){
-                Text("Create account")
-                    .font(.headline)
-                    .padding()
-                    .frame(maxWidth: .infinity)
-                    .background(Color.blue)
-                    .foregroundColor(.white)
-                    .cornerRadius(50)
-            }.padding(.top)
+                }.padding(.top)
+                
+                Text("or")
 
-            Text("By signing up, you agree to the Terms of Servie and Privacy Policy, including Cookie Use.")
-                .font(.callout)
-            Spacer()
-
-            
-            HStack {
-                Text("Have an account already?")
-                Text("Log in")
-                    .foregroundColor(.blue)
+                NavigationLink(destination: HomeScreenView()) {
+                                    Text("Create account")
+                                        .font(.headline)
+                                        .padding()
+                                        .frame(maxWidth: .infinity)
+                                        .background(Color.blue)
+                                        .foregroundColor(.white)
+                                        .cornerRadius(50)
+                                }
+                                .padding(.top)
+                
+                Text("By signing up, you agree to the Terms of Servie and Privacy Policy, including Cookie Use.")
+                    .font(.callout)
+                Spacer()
+                
+                
+                HStack {
+                    Text("Have an account already?")
+                    Text("Log in")
+                        .foregroundColor(.blue)
+                }
+                .font(.none)
+                
             }
-            .font(.none)
-
+            .padding(22)
         }
-        .padding(22)
     }
 }
 
---
+++ TwitterClone/homeScreen.swift
@@ -0,0 +1,23 @@
+//
+//  homeScreen.swift
+//  TwitterClone
+//
+//  Created by farhan haider on 25/08/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct HomeScreenView: View {
+    var body: some View {
+        VStack{
+            Text("hi")
+        }
+        
+    }
+        
+}
+
+#Preview {
+    HomeScreenView()
+}
