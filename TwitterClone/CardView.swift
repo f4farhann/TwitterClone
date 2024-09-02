66269082d290e2aee230d45e4da1c941e54afc1c
farhan haider <f4farhann@gmail.com>
2024-09-03 00:33:28 +0530

    update cardview

--- TwitterClone/CardView.swift
+++ TwitterClone/CardView.swift
@@ -16,32 +16,48 @@ struct CardView: View {
                 .frame(width: 60, height: 60)
             VStack(alignment: .leading){
                 HStack{
-                    Text("NAME")
-                    Image(systemName: "tickmark")
-                    Text("USERID")
-                    Text("1.d")
+                    Text("User Name")
+                    Image(systemName: "checkmark.seal.fill")
+                    Text("@userId • 1d")
                     Image(systemName: "ellipsis")
-
+                        .rotationEffect(.degrees(90))
+                    
                 }
-                Text("CONTENT TEXT")
+                Text("I'm deleting this soon because it's a legit cash-printing formula.")
+                            .font(.body)
                 Image("google_logo")
                     .resizable()
                     .scaledToFit()
                     .frame(width: 100, height: 100)
                 
                 HStack{
-                      Image(systemName: "message")
-                      Image(systemName: "arrow.2.squarepath")
-                      Image(systemName: "heart")
-                      Image(systemName: "chart.bar")
-                      Image(systemName: "bookmark")
-                      Image(systemName: "arrowshape.turn.up.forward")
+                    IconView(iconName: "message", text: "2.4k")
+                       IconView(iconName: "arrow.2.squarepath", text: "2.4k")
+                       IconView(iconName: "heart", text: "2.4k")
+                       IconView(iconName: "chart.bar", text: "2.4k")
+                       IconView(iconName: "bookmark")
+                       IconView(iconName: "arrowshape.turn.up.forward")
                 }
                 
             }
         }
     }
 }
+
+struct IconView: View {
+    var iconName: String
+    var text: String?
+
+    var body: some View {
+        HStack(spacing: 4) {
+            Image(systemName: iconName)
+            if let text = text {
+                Text(text)
+            }
+        }
+    }
+}
+
 
 #Preview {
     CardView()
